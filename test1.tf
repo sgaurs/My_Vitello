5b64c0b1e5c4cdefecc5830f6b6ac17ba69e1c23
vatsaarush <jyotisharma23121@gmail.com>
2025-08-02 15:10:16 +0530

    initial code

---
+++ test1.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+  features {}
+}
+
+resource "azurerm_resource_group" "Stest_rg" {
+  name     = "Sumit_RG_terraform1234"
+  location = "West US"
+}
